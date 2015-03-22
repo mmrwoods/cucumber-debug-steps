64d0f46a87d311ab5a1ae30c309e91bb66d8d74d
Mark Woods <mwoods@online.ie>
2015-03-22 13:09:06 +0000

    Warn if no compatible debugger found
    Note: explicitly check for Debugger or Byebug because active support
    adds a debugger method to Kernel if missing, and just logs a warning.

--- lib/cucumber/debug_steps.rb
+++ lib/cucumber/debug_steps.rb
@@ -1,6 +1,10 @@
 require 'coderay'
 require 'htmlbeautifier'
 
+unless defined?(Debugger) || defined?(Byebug)
+  $stderr.puts("WARNING: cucumber-debug-steps requires a ruby debugger, please install debugger gem for ruby 1.9 or byebug for ruby 2")
+end
+
 module DebugStepsHelper
   def peek
     print CodeRay.scan(HtmlBeautifier.beautify(page.source), :html).term, "\n"
